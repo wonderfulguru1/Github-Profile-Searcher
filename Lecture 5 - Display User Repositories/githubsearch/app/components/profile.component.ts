f8ee6eacde4669d96f1a7968efdc6aca9fc7df05
Temiye Oluseun <temiye.oluseun@duemarketing.com>
2017-03-28 19:23:58 +0100

    first commit

---
+++ Lecture 5 - Display User Repositories/githubsearch/app/components/profile.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {GithubService} from '../services/github.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'profile',
+    templateUrl: 'profile.component.html'
+})
+
+export class ProfileComponent { 
+    user:any[];
+    repos:any[];
+    
+    constructor(private _githubService: GithubService){
+        this._githubService.getUser().subscribe(user => {
+            this.user = user;
+        });
+        
+        this._githubService.getRepos().subscribe(repos => {
+            this.repos = repos;
+        });
+    }
+}
